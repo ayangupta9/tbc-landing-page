ea9c4911f52d2c2b4636210bdf07c4175ac8b089
Ayan Gupta <ayangupta1000@gmail.com>
2024-04-10 12:58:34 +0200

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/tbc-landing-page/' 
-    : '/'
+  base: '/tbc-landing-page/' 
 });
